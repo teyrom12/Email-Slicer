a58b159e0ee80c1cb456f7c87f846fd6a8798072
Antonesei Albert <86300499+teyrom12@users.noreply.github.com>
2022-11-13 14:55:15 +0200

    Update Email_Slicer.py

--- Email_Slicer.py
+++ Email_Slicer.py
@@ -1,5 +1,30 @@
-email = input("Enter you Email: ").strip()
-username = email[:email.index("@")]
-domain_name = email[email.index("@")+1:]
-format_ = (f"Your user name is '{username}' and your domain is '{domain_name}'")
-print(format_)
+from tkinter import *
+from tkinter import ttk
+
+win = Tk()
+win.geometry("700x250")
+win.title('Email Slicer')
+
+def get_username():
+   email = entry.get()
+   username = email[:email.index("@")]
+   return username
+
+def get_domain():
+    email = entry.get()
+    domain = email[email.index("@")+1:]
+    return domain
+    
+def get_data():
+   label.config(text= "The user name is: " + get_username() + ", and the domain is: " + get_domain(), font= ('Helvetica 13'))
+
+entry = Entry(win, width= 42)
+entry.place(relx= .5, rely= .5, anchor= CENTER)
+
+
+label= Label(win, text="", font=('Helvetica 13'))
+label.pack()
+
+ttk.Button(win, text= "Split Email", command= get_data).place(relx= .7, rely= .5, anchor= CENTER)
+
+win.mainloop()
